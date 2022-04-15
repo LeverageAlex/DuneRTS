2df18286651a8e41be3d6643b0fa0951e9add5e0
Alexander <alexander.fink@uni-ulm.de>
2022-04-15 18:31:18 +0200

    feat: CharacterManager now adds Chars

--- Unity/Dune/Assets/Scripts/CharacterMgr.cs
+++ Unity/Dune/Assets/Scripts/CharacterMgr.cs
@@ -12,21 +12,39 @@ public class CharacterMgr : MonoBehaviour
     public GameObject beneGesseritPrefab;
     public GameObject fighterPrefab;
 
+
+    public enum charType
+    {
+        NOBLE, MENTANT, BENEGESSERIT, FIGHTER
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Example to show Method
+        //spawnCharacter(175, "FIGHTER", 15, 15, 4, 10, 4, 4, 3, 6, false, false);
+   
     }
+
+
+
 
 
     /*
      * To be filled after open question regarding standardDocument has ben resolved
-  /*  public bool spawnEnemy()
+  */  public bool spawnCharacter(int characterID, string type,int x, int z,int HPcurrent, int healingHP, int MPcurrent, int APcurrent, int attackDamage, int inventoryLeft, bool killedBySandworm, bool loud)
     {
-        if (!characterDict.ContainsKey(characterID))
+        if (characterDict.ContainsKey(characterID))
             return false;
 
-    }*/
+
+
+        GameObject newChar = (GameObject) Instantiate(getCharTypeByString(type), new Vector3(x, 0f, z), Quaternion.identity);
+        characterDict.Add(characterID, newChar);
+        Character localChar = (Character) newChar.GetComponent(typeof(Character));
+        localChar.updateCharStats(HPcurrent, healingHP, MPcurrent, APcurrent, attackDamage, inventoryLeft, loud, killedBySandworm);
+        return true;
+    }
 
     public bool characterStatChange(int characterID, int HP, int HealHP, int MP, int AP, int AD, int spiceInv, bool isLoud, bool isSwallowed)
     {
@@ -41,6 +59,25 @@ public class CharacterMgr : MonoBehaviour
     public Character getCharScriptByID(int characterID)
     {
         return (Character)characterDict[characterID].GetComponent(typeof(Character));
+    }
+
+
+    GameObject getCharTypeByString(string type)
+    {
+        switch (type)
+        {
+            case "NOBLE":
+                return noblePrefab;
+            case "BENEGESSERIT":
+                return beneGesseritPrefab;
+            case "MENTAT":
+                return mentatPrefab;
+            case "FIGHTER":
+                return fighterPrefab;
+            default:
+                Debug.Log("Error in CharacterMgr: CharType doesn't exist!");
+                return null;
+        }
     }
 
 
