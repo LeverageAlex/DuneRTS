dfd9b88036cef4f01ed918d6e43f1fccc5bc0227
felix schoppe <felix.schoppe@uni-ulm.de>
2022-05-12 14:56:01 +0200

    Implemented new Messages

---
+++ DesertsOfDune/GameData/network/messages/DebugMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameData.network.messages
+{
+    /// <summary>
+    /// This Class is used for Debug Messages.
+    /// </summary>
+    public class DebugMessage : Message
+    {
+        private int code;
+        private string explanation;
+
+        /// <summary>
+        /// Constructor of the Class DebugMessage
+        /// </summary>
+        /// <param name="code">the code of the DebugMessage</param>
+        /// <param name="explanation">the explanation for the DebugMessage</param>
+        public DebugMessage(int code, string explanation) : base("v1", Enums.MessageType.DEBUG)
+        {
+            this.code = code;
+            this.explanation = explanation;
+        }
+
+        /// <summary>
+        /// Constructof of the class DebugMessage that only take the explanation
+        /// </summary>
+        /// <param name="explanation">The explanation for the DebugMessage</param>
+        public DebugMessage(string explanation) : base("v1", Enums.MessageType.DEBUG)
+        {
+            this.explanation=explanation;
+        }
+    }
+}
