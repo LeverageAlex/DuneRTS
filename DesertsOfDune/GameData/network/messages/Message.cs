fb6e90323acaeceeb644c654151107e880088a85
felix schoppe <felix.schoppe@uni-ulm.de>
2022-05-12 13:57:29 +0200

    Implemented Class Message and Messagetype enum.

---
+++ DesertsOfDune/GameData/network/messages/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameData.network.messages
+{
+    public abstract class Message
+    {
+        private int version;
+        private Enums.MessageType type;
+
+        public Message(int version, Enums.MessageType type)
+        {
+            this.version = version;
+            this.type = type;
+        }
+
+        public Enums.MessageType getMessageType()
+        {
+            return this.type;
+        }
+    }
+}
