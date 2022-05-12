b81bea9f140e7bd181e6e7f3050096724bee2745
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-05-12 13:45:15 +0200

    feat(network): add template for a class, that should can convert Strings into Message-Objects (and reverse)

---
+++ DesertsOfDune/GameData/network/util/parser/MessageConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameData.network.util.parser
+{ 
+    static public class MessageConverter
+    {
+        static public String FromMessage(Message message)
+        {
+            return "";
+        }
+
+        static public Message ToMessage(String message)
+        {
+            return null;
+        }
+    }
+}
