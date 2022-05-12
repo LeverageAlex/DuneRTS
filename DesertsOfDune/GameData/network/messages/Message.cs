205df77f5db08727a5b9137fde9eb9dfa2167780
felix schoppe <felix.schoppe@uni-ulm.de>
2022-05-12 14:36:01 +0200

    Implemented createMessage

--- DesertsOfDune/GameData/network/messages/Message.cs
+++ DesertsOfDune/GameData/network/messages/Message.cs
@@ -4,17 +4,28 @@ using System.Text;
 
 namespace GameData.network.messages
 {
+    /// <summary>
+    /// The base Message class all Messages extend this class.
+    /// </summary>
     public abstract class Message
     {
-        private int version;
-        private Enums.MessageType type;
+        protected string version;
+        protected Enums.MessageType type;
 
-        public Message(int version, Enums.MessageType type)
+        /// <summary>
+        /// The base constructor of the class Message
+        /// </summary>
+        /// <param name="version">version of the Message</param>
+        /// <param name="type">the Messagetype of the Message</param>
+        public Message(string version, Enums.MessageType type)
         {
             this.version = version;
             this.type = type;
         }
-
+        /// <summary>
+        /// Getter forthe field type
+        /// </summary>
+        /// <returns>Messagetype</returns>
         public Enums.MessageType getMessageType()
         {
             return this.type;
