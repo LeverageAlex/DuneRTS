d71725ecacc3dfd44068506a39d2647e8276493d
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-05-13 15:39:15 +0200

    feat(network): add abstract class for the connection handler (common properties of the connection handler from the server and the client)

---
+++ DesertsOfDune/GameData/network/controller/ConnectionHandler/AConnectionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using WebSocketSharp;
+
+namespace GameData.network.controller
+{
+    abstract public class AConnectionHandler
+    {
+        private String SERVER_ADDRESS { get; }
+        private int PORT { get; }
+
+        private String PROTOCOLL_SUFFIX = "ws://";
+
+        protected AConnectionHandler(string ServerAddress, int Port)
+        {
+            SERVER_ADDRESS = ServerAddress;
+            PORT = Port;
+        }
+
+        protected String GetURL()
+        {
+            return this.PROTOCOLL_SUFFIX + this.SERVER_ADDRESS + ":" + this.PORT;
+        }
+
+        abstract protected internal void OnOpen();
+
+        abstract protected internal void OnClose(CloseEventArgs e);
+
+        abstract protected internal void OnError(ErrorEventArgs e);
+
+        abstract protected internal void OnMessage(MessageEventArgs e);
+    }
+}
