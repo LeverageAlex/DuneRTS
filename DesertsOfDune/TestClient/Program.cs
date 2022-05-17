e0efccf189e7c8e71cd97e7dc78a0cfdaa77ca47
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-05-17 12:48:54 +0200

    add dummy client project for testing purpose (messages and sending them via network)

---
+++ DesertsOfDune/TestClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using GameData.network.controller;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Hello World 2!");
+
+            MessageController messageController2 = new MessageController();
+
+            ClientConnectionHandler clientConnectionHandler = new ClientConnectionHandler("127.0.0.1", 7890);
+
+            ClientNetworkController clientNetworkController = new ClientNetworkController(clientConnectionHandler, messageController2);
+
+            // client
+
+            messageController2.DoDebug("Hallo emil, hier ist Alex");
+        }
+    }
+}
