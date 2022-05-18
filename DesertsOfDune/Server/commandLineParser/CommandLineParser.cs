3fbb0dbb29249762f1287a66cc7d62f15e7165ab
felix schoppe <felix.schoppe@uni-ulm.de>
2022-05-18 12:22:24 +0200

    feat: Implemented Class CommandLineParser without logic

---
+++ DesertsOfDune/Server/commandLineParser/CommandLineParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.commandLineParser
+{
+    /// <summary>
+    /// This class is responsible for parsing commandline input.
+    /// </summary>
+    public static class CommandLineParser
+    {
+        /// <summary>
+        /// This method prints all commandline attributes and theyr meaning to the commandLine.
+        /// </summary>
+        public static void PrintHelp()
+        {
+
+        }
+
+        /// <summary>
+        /// This method parses the argument and triggers its handling
+        /// </summary>
+        /// <param name="parameter">the parameter that should be parsed and handeld</param>
+        public static void ParseArgument(string parameter)
+        {
+
+        }
+    }
+}
