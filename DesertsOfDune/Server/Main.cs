e3e9b18d2cc3b1df2ad124223160b11f7d1094bf
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-05-18 17:16:36 +0200

    feat(server): created main class for the server and created logger and network module in it

---
+++ DesertsOfDune/Server/Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using GameData.network.controller;
+using GameData.network.util;
+using Serilog;
+
+namespace Server
+{
+    static class Programm {
+
+        /// <summary>
+        /// main method, which is executed when the server was started / executed
+        /// It will initialize all classes and trigger all starting events including
+        /// <list type="bullet">
+        /// <item>create and configure network module</item>
+        /// <item>load configuration files and configure everything</item>
+        /// <item>create and configure logger</item>
+        /// </list>
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            InitiliazeLogger();
+            Log.Information("Starting server... Everything will be created and configured.");
+
+            MessageController messageController = new MessageController();
+            
+            ServerConnectionHandler serverConnectionHandler = new ServerConnectionHandler("127.0.0.1", 7890);
+
+            ServerNetworkController serverNetworkController = new ServerNetworkController(serverConnectionHandler, messageController);
+
+            // server
+            messageController.DoDebug(113, "Hallo hier ist Günther");
+
+
+            // server
+            // messageController.DoDebug("Alex ist sehr fleißig");
+        }
+
+        /// <summary>
+        /// creates and configures a new logger for the server
+        /// </summary>
+        private static void InitiliazeLogger()
+        {
+            Logger.CreateDefaultLogger();
+        }
+    }
+}
