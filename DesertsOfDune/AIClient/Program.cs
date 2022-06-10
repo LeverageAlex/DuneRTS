87407c89c6862b3e0c3cdb5a2839549c6238f14b
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-06-10 23:19:28 +0200

    feat(ai_client): created new project for the ai client (console application)

---
+++ DesertsOfDune/AIClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
