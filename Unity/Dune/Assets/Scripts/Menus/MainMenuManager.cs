5d31059f1cc7a4e1a396b88c464c6307a7ccb552
Louis Bock <louis.bock@uni-ulm.de>
2022-06-11 12:52:08 +0200

    refactor: removed create game funktionality

--- Unity/Dune/Assets/Scripts/Menus/MainMenuManager.cs
+++ Unity/Dune/Assets/Scripts/Menus/MainMenuManager.cs
@@ -10,21 +10,15 @@ public class MainMenuManager : MonoBehaviour
     public GameObject MainMenu;
     public GameObject OptionsMenu;
     public GameObject PlayOptionsMenu;
-    public GameObject CreateJoinGameMenu;
+    public GameObject JoinGameMenu;
 
     [Header("Create/Join Game:")]
-    public GameObject createGameText;
     public GameObject joinGameText;
-    public GameObject cpuCountPanel;
-    public GameObject createButton;
     public GameObject joinButton;
     public InputField nameInput;
     public Toggle playerToggle;
     public Toggle viewerToggle;
     public InputField lobbyCodeInput;
-    public Dropdown cpuCountDropdown;
-    //public Dropdown scenarioDropdown;
-    //public Dropdown matchDropdown;
 
     private string clientSecret;
 
@@ -47,26 +41,6 @@ public class MainMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    /// <summary>
-    /// this mehtod is called by a BUTTON to create a game
-    /// </summary>
-    public void CreateGame()
-    {
-        string lobbyCode = lobbyCodeInput.text;
-        int cpuCount = cpuCountDropdown.value;
-
-        string name = nameInput.text;
-        bool active = playerToggle.isOn;
-
-        //TODO validate inputs
-
-        Debug.Log("Create: " + lobbyCode + " " + cpuCount);
-
-        //TODO send CREATE message to server
-
-        JoinGame(name, lobbyCode, active);
     }
 
     /// <summary>
@@ -118,7 +92,7 @@ public class MainMenuManager : MonoBehaviour
         MainMenu.SetActive(false);
         OptionsMenu.SetActive(false);
         PlayOptionsMenu.SetActive(false);
-        CreateJoinGameMenu.SetActive(false);
+        JoinGameMenu.SetActive(false);
 
         if (menuToActivate != null)
         {
@@ -154,28 +128,10 @@ public class MainMenuManager : MonoBehaviour
     }
 
     /// <summary>
-    /// this method is called by a BUTTON to switch to the CreateGameMenu
-    /// </summary>
-    public void SwitchToCreatGameMenu()
-    {
-        ActivateMenu(CreateJoinGameMenu);
-        createGameText.SetActive(true);
-        joinGameText.SetActive(false);
-        cpuCountPanel.SetActive(true);
-        createButton.SetActive(true);
-        joinButton.SetActive(false);
-    }
-
-    /// <summary>
     /// this method is called by a BUTTON to switch to the JoinGameMenu
     /// </summary>
     public void SwitchToJoinGameMenu()
     {
-        ActivateMenu(CreateJoinGameMenu);
-        createGameText.SetActive(false);
-        joinGameText.SetActive(true);
-        cpuCountPanel.SetActive(false);
-        createButton.SetActive(false);
-        joinButton.SetActive(true);
+        ActivateMenu(JoinGameMenu);
     }
 }
