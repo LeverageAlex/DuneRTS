e2e8de6ebf17c5c8466519bb171c6af71a12aa58
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-06-20 12:08:38 +0200

    feat8ai_client): added classes for all possible 'moves', so movement or any action

---
+++ DesertsOfDune/AIClient/Moves/Action.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using GameData.network.messages;
+using Serilog;
+
+namespace AIClient.Moves
+{
+    /// <summary>
+    /// Represents the "move" to move a character in a direction
+    /// </summary>
+    public class Action : Move
+    {
+        public ActionType actionType;
+
+        public Action(MoveTypes type) : base(type)
+        {
+            switch(type){
+                case MoveTypes.ATTACK:
+                    actionType = ActionType.ATTACK;
+                    break;
+                case MoveTypes.COLLECT_SPICE:
+                    actionType = ActionType.COLLECT;
+                    break;
+                case MoveTypes.TRANSFER_SPICE:
+                    actionType = ActionType.TRANSFER;
+                    break;
+                case MoveTypes.KANLY:
+                    actionType = ActionType.KANLY;
+                    break;
+                case MoveTypes.FAMILY_ATOMICS:
+                    actionType = ActionType.FAMILY_ATOMICS;
+                    break;
+                case MoveTypes.SPICE_HOARDING:
+                    actionType = ActionType.SPICE_HOARDING;
+                    break;
+                case MoveTypes.VOICE:
+                    actionType = ActionType.VOICE;
+                    break;
+                case MoveTypes.SWORD_SPIN:
+                    actionType = ActionType.SWORD_SPIN;
+                    break;
+                default:
+                    Log.Error($"The given MoveType {type} is invalid as a character action!");
+                    break;
+            }
+        }
+    }
+}
---
+++ DesertsOfDune/AIClient/Moves/Movement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace AIClient.Moves
+{
+    /// <summary>
+    /// Represents the "move" to move a character in a direction
+    /// </summary>
+    public class Movement : Move
+    {
+        public int DeltaX { get; }
+        public int DeltaY { get; }
+
+        /// <summary>
+        /// creates a new movement move and set the delta x and y movement of this move depending on the type
+        /// </summary>
+        /// <param name="type">the movement type respectivly the movement direction</param>
+        public Movement(MoveTypes type) : base(type)
+        {
+            // TODO: check, whether dy = -1 really is moving upwards
+            switch (type)
+            {
+                case MoveTypes.MOVE_LEFT_UP:
+                    DeltaX = -1;
+                    DeltaY = -1;
+                    break;
+                case MoveTypes.MOVE_UP:
+                    DeltaX = 0;
+                    DeltaY = -1;
+                    break;
+                case MoveTypes.MOVE_RIGHT_UP:
+                    DeltaX = 1;
+                    DeltaY = -1;
+                    break;
+                case MoveTypes.MOVE_RIGHT:
+                    DeltaX = 1;
+                    DeltaY = 0;
+                    break;
+                case MoveTypes.MOVE_RIGHT_DOWN:
+                    DeltaX = 1;
+                    DeltaY = 1;
+                    break;
+                case MoveTypes.MOVE_DOWN:
+                    DeltaX = 0;
+                    DeltaY = 1;
+                    break;
+                case MoveTypes.MOVE_LEFT_DOWN:
+                    DeltaX = -1;
+                    DeltaY = 1;
+                    break;
+                case MoveTypes.MOVE_LEFT:
+                    DeltaX = -1;
+                    DeltaY = 0;
+                    break;
+                default:
+                    Log.Error($"The given MoveType {type} is invalid as a movement direction!");
+                    break;
+            }
+        }
+    }
+}
