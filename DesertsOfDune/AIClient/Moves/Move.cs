41831f5536ce134efa1f2835c98a8a5c957d5372
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-06-20 11:55:53 +0200

    feat(ai_client): added move class and all possible move types

---
+++ DesertsOfDune/AIClient/Moves/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AIClient.Moves
+{
+    /// <summary>
+    /// Represents the general idea of a "move", so either the demand to end the turn, do an action or move in any direction
+    /// </summary>
+    public abstract class Move
+    {
+        public MoveTypes Type { get; private set; }
+
+        public Move(MoveTypes type)
+        {
+            this.Type = type;
+        }
+    }
+}
