6a1c5bd11b9771057a2e334d5b140504dcf2f2ba
Leonhard Alkewitz <leonhard.alkewitz@uni-ulm.de>
2022-06-23 23:00:11 +0200

    feat(test_project): added setup class and method for setting up a 'demo party' with a configuration, network module etc.

---
+++ DesertsOfDune/TestProject/Setup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using GameData;
+using GameData.Configuration;
+using GameData.network.controller;
+using NUnit.Framework;
+
+namespace UnitTestSuite
+{
+    /// <summary>
+    /// Base class for the Unit Tests containing the setup
+    /// </summary>
+    /// <remarks>
+    /// This is necessary, because most of the tests need the same setup consisting of
+    /// <list type="bullet">
+    /// <item>creating all configurations</item>
+    /// <item>creating a dummy network controller and message controller</item>
+    /// </list>
+    /// With inheriting this class, all unit tests can easily use the setup method and do their basic setup and add further if needed
+    /// </remarks>
+    public class Setup
+    {
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Setup()
+        {
+        }
+
+        /// <summary>
+        /// set up the configuration classes and a basic network module as well as a party containing all information
+        /// </summary>
+        [SetUp]
+        public void NetworkAndConfigurationSetUp()
+        {
+            ConfigurationFileLoader loader = new ConfigurationFileLoader();
+
+            // load scenario and create a new scenario configuration
+            ScenarioConfiguration scenarioConfiguration = loader.LoadScenarioConfiguration("./ConfigurationFiles/team08.scenario.json");
+            ScenarioConfiguration.CreateInstance(scenarioConfiguration);
+
+            // load the party configuration and create a new party configuration class
+            PartyConfiguration partyConfiguration = loader.LoadPartyConfiguration("./ConfigurationFiles/team08.party.json");
+            PartyConfiguration.SetInstance(partyConfiguration);
+
+            // initialization for greatHouses in GameData project
+            Configuration.InitializeConfigurations();
+            // initialization for the character configurations in GameData project
+            Configuration.InitializeCharacterConfiguration(
+                PartyConfiguration.GetInstance().noble,
+                PartyConfiguration.GetInstance().mentat,
+                PartyConfiguration.GetInstance().beneGesserit,
+                PartyConfiguration.GetInstance().fighter);
+
+            // set up a dummy network module running on 127.0.0.1:8000
+
+            // TODO: eventually change to message controller
+            ServerMessageController messageController = new ServerMessageController();
+
+            string serverAddress = "127.0.0.1";
+            int port = 80000;
+
+            ServerConnectionHandler serverConnectionHandler = new ServerConnectionHandler(serverAddress, port);
+            _ = new ServerNetworkController(serverConnectionHandler, messageController);
+
+            // create a new party and set the message controller
+            Party.GetInstance().messageController = messageController;
+        }
+    }
+}
+
