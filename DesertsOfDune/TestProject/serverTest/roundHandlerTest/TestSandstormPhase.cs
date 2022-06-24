f1f5821120c94aec0e13db6ee7f02fcecdbd48cd
Louis Bock <louis.bock@uni-ulm.de>
2022-06-24 22:55:57 +0200

    feat: TestSandstormPhase

--- DesertsOfDune/TestProject/serverTest/roundHandlerTest/TestSandstormPhase.cs
+++ DesertsOfDune/TestProject/serverTest/roundHandlerTest/TestSandstormPhase.cs
@@ -11,41 +11,23 @@ using GameData.network.util.world.mapField;
 using GameData.network.util.world.character;
 using GameData.Configuration;
 using GameData;
+using GameData.network.util.enums;
 
 namespace UnitTestSuite.serverTest.roundHandlerTest
 {
     /// <summary>
     /// This Class is used to Test the class SandstormPhase
     /// </summary>
-    public class TestSandstormPhase
+    public class TestSandstormPhase : Setup
     {
         private Map map;
 
         [SetUp]
         public void Setup()
         {
-            ConfigurationFileLoader loader = new ConfigurationFileLoader();
+            base.NetworkAndConfigurationSetUp();
 
-            // load scenario and create a new scenario configuration
-            ScenarioConfiguration scenarioConfiguration = loader.LoadScenarioConfiguration("../.././../ConfigurationFiles/team08.scenario.json");
-            ScenarioConfiguration.CreateInstance(scenarioConfiguration);
-
-            // load the party configuration and create a new party configuration class
-            PartyConfiguration partyConfiguration = loader.LoadPartyConfiguration("../.././../ConfigurationFiles/team08.party.json");
-            PartyConfiguration.SetInstance(partyConfiguration);
-
-            //Initialization for greatHouses in GameData project
-            GameData.Configuration.Configuration.InitializeConfigurations();
-            // Initialization for the character configurations in GameData project
-            GameData.Configuration.Configuration.InitializeCharacterConfiguration(
-                PartyConfiguration.GetInstance().noble,
-                PartyConfiguration.GetInstance().mentat,
-                PartyConfiguration.GetInstance().beneGesserit,
-                PartyConfiguration.GetInstance().fighter);
-
-            
             map = new Map(ScenarioConfiguration.SCENARIO_WIDTH, ScenarioConfiguration.SCENARIO_HEIGHT, ScenarioConfiguration.GetInstance().scenario);
-
         }
 
         /// <summary>
@@ -88,22 +70,31 @@ namespace UnitTestSuite.serverTest.roundHandlerTest
             Assert.False(eye.XCoordinate == sp.EyeOfStorm.XCoordinate && eye.ZCoordinate == sp.EyeOfStorm.ZCoordinate);
         }
 
-        /// <summary>
-        /// This Testcase validates the behaviour of the method CalculateNewStrom
-        /// </summary>
+      
         [Test]
-        public void TestCalculateNewStrom()
+        public void TestExecute()
         {
-            // implement logic
-        }
-
-        [Test]
-        public void Execute()
-        {
+            //create SandstormPhase
+            SandstormPhase sp = new SandstormPhase(map);
             
+            sp.Execute();
 
+            //check for sandStormTiles
+            List<MapField> fields = new List<MapField>(map.GetSandstormFieldsOnMap());//GetSandstormFieldsOnMap is correctly testet
 
+            Assert.True(fields.Count > 0);
 
+            Assert.True(fields.Contains(sp.EyeOfStorm));
+            fields.Remove(sp.EyeOfStorm);
+
+            foreach (MapField f in map.GetNeighborFields(sp.EyeOfStorm))//GetNeighborFields is correctly tested
+            {
+                Assert.True(fields.Contains(f));
+                fields.Remove(f);
+            }
+
+            Assert.True(fields.Count == 0);
         }
+
     }
 }
