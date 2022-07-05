6bcbe77bed6dd597003ab06e8295834fd48765f1
felix schoppe <felix.schoppe@uni-ulm.de>
2022-07-05 14:29:28 +0200

    Codecleanup for the class Specs.

--- DesertsOfDune/GameData/network/messages/Specs.cs
+++ DesertsOfDune/GameData/network/messages/Specs.cs
@@ -4,6 +4,9 @@ using System;
 using Newtonsoft.Json;
 using GameData.network.util.world;
 
+/// <summary>
+/// This class is used to support different kinds of messages.
+/// </summary>
 public class Specs
 {
     [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
@@ -11,11 +14,15 @@ public class Specs
     [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public Position target;
 
+    /// <summary>
+    /// Constructor of the Class Specs
+    /// </summary>
+    /// <param name="target">defines the target Position of a action</param>
+    /// <param name="path">defines the path of a action</param>
     [JsonConstructor]
     public Specs(Position target, List<Position> path )
     {
         this.path = path;
         this.target = target;
-
     }
 }
